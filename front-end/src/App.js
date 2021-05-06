f19bbbec00ddd00fcd401369f6bc4418ebacc7a6
bryg217 <bryan.guillen217@gmail.com>
2021-05-06 17:12:21 -0400

    feat: add support request for ideas on load
    Note: This is primitive support.  This will be updated in following commits

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [ideas, setIdeas] = useState([]);
+  const [lastIdeaSeen, setLastIdeaSeen] = useState('null');
+
+  /**
+   * @description Effect for loading data
+   */
+  useEffect(() => {
+    axios
+      .get(`/ideas?lid=${lastIdeaSeen}`)
+      .then(response => {
+        const { results } = response.data;
+        setIdeas(results);
+      })
+      .catch(error => {
+        /**
+         * @TODO (Urgent) Do something with the error for production
+         */
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>Hello world</div>
   );
