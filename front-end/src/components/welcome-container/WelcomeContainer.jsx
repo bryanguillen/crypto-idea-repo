bfcb0e324483760dfa6f1b54364ac0b9e7e0037d
bryg217 <bryan.guillen217@gmail.com>
2021-05-07 09:27:30 -0400

    feat: add support for new prop, openIntroMoal

--- front-end/src/components/welcome-container/WelcomeContainer.jsx
+++ front-end/src/components/welcome-container/WelcomeContainer.jsx
@@ -1,11 +1,11 @@
 import './WelcomeContainer.css';
 
-export default function WelcomeContainer() {
+export default function WelcomeContainer(props) {
   return (
     <div className="welcome-container">
       <div className="header-2-bold">Hodlers, Help Build the Future of Crypto.</div>
       <div className="welcome-container-buttons-container">
-        <button className="welcome-container-button welcome-container-button-start">New? Start Here</button>
+        <button className="welcome-container-button welcome-container-button-start" onClick={() => props.openIntroModal()}>New? Start Here</button>
         <button className="welcome-container-button welcome-container-button-share">Share Idea</button>
       </div>
     </div>
