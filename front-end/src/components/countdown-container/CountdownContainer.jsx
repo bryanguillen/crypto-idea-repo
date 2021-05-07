2e9269f762a2f93c4654bc1f69371dfe0b495e5c
bryg217 <bryan.guillen217@gmail.com>
2021-05-07 17:13:30 -0400

    chore: update copy

--- front-end/src/components/countdown-container/CountdownContainer.jsx
+++ front-end/src/components/countdown-container/CountdownContainer.jsx
@@ -7,7 +7,8 @@ import './CountdownContainer.css';
 export default function CountdownContainer() {
   return (
     <div className="countdown-container">
-      <div className="header-2-bold">Next Giveaway: 05/21/21 @ 12 PM EST</div>
+      <div className="header-2-bold">Next Giveaway: 05/15/21 @ 12 PM EST</div>
+      <div className="header-2-bold">Prize: 50$ (USD) In ADA or BTC (your choice)</div>
     </div>
   );
 }
