9b41c101cded94256bf8f88a99380961e7c7ad7e
bryg217 <bryan.guillen217@gmail.com>
2021-05-07 15:18:50 -0400

    feat: initial commit

---
+++ front-end/src/components/countdown-container/CountdownContainer.jsx
@@ -0,0 +1,13 @@
+import './CountdownContainer.css';
+
+/**
+ * @description Function component for countdown
+ * @returns {Object}
+ */
+export default function CountdownContainer() {
+  return (
+    <div className="countdown-container">
+      <div className="header-2-bold">Next Giveaway: 05/21/21 @ 12 PM EST</div>
+    </div>
+  );
+}
