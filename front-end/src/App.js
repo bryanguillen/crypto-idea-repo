df0e86bcfde6653159d440727ec02a59bfcce84f
bryg217 <bryan.guillen217@gmail.com>
2021-05-07 14:56:04 -0400

    feat: add support for infinite scrolling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import ReactModal from 'react-modal'
 
@@ -11,12 +11,17 @@ import './App.css';
 
 function App() {
   const [ideas, setIdeas] = useState([]);
+  const [infiniteScrollCounter, setInfiniteScrollCounter] = useState(0);
   const [introModalVisible, setIntroModalVisible] = useState(false);
   const [lastIdeaSeen, setLastIdeaSeen] = useState('null');
   const [loading, setLoading] = useState(true);
+  const [olderIdeasExist, setOlderIdeasExist] = useState(true);
   const [shareIdeaModalVisible, setShareIdeaModalVisible] = useState(false);
   const [shareLink, setShareLink] = useState('');
-
+  
+  // Used for infinite scroll
+  const bottomBoundary = useRef(null);
+  
   // Set App Element For All Modals
   ReactModal.setAppElement('#root');
 
@@ -27,8 +32,9 @@ function App() {
     axios.get(`/ideas?lid=${lastIdeaSeen}`)
       .then(response => {
         const { results } = response.data;
-        setIdeas(results);
+        setIdeas(previousState => [...previousState, ...results]);
         setLastIdeaSeen(results[results.length - 1].id);
+        setOlderIdeasExist(results.length === 10);
         if (loading) {
           setLoading(false);
         }
@@ -36,7 +42,7 @@ function App() {
       .catch(() => {
         setLoading(false);
       });
-  }, []);
+  }, [infiniteScrollCounter]);
 
   /**
    * @description Effect for incrementing referral count on load
@@ -50,6 +56,21 @@ function App() {
         .catch(error => console.log(error));
     }
   }, []);
+
+  /**
+   * @description Scroll observer
+   * @param {Object} node
+   * @returns {}
+   */
+  function handleIntersection(node) {
+    new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          setInfiniteScrollCounter(previousState => previousState + 1);
+        }
+      });
+    }).observe(node)
+  }
 
   /**
    * @description Handler for clicking upvote button
@@ -126,6 +147,16 @@ function App() {
           handleUpvoteClick={handleUpvoteClick}
           ideas={ideas}
         />
+        {
+          olderIdeasExist ?
+            <div ref={element => {
+              if (element !== null && bottomBoundary.current === null) {
+                bottomBoundary.current = element;
+                handleIntersection(element);
+              }
+            }}></div> :
+            null
+        }
       </div> :
       <div>loading...</div>
   );
