d34d51462d857dce715697ee77c22ad732cc2716
bryg217 <bryan.guillen217@gmail.com>
2021-05-06 21:44:03 -0400

    feat: add support for upvote button and functionality

--- front-end/src/components/idea/Idea.jsx
+++ front-end/src/components/idea/Idea.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+import { IconContext } from 'react-icons';
+import { IoArrowUpCircleOutline } from 'react-icons/io5';
+
 import './Idea.css';
 
 export default function Idea({
   context,
   description,
-  title
+  title,
+  upvoteIdea
 }) {
+  const [upvoteIsActive, setUpvoteIsActive] = useState(false);
+
+  /**
+   * @description Function used to handle the click for upvote;
+   * it is used to wrap the prop function passed in the changing of color
+   */
+  function handleUpvoteClick() {
+    upvoteIdea();
+    setUpvoteIsActive(true);
+  }
+
   return (
     <div className="idea">
       <div className="header-2-bold">{title}</div>
@@ -15,6 +31,14 @@ export default function Idea({
       <div className="idea-section">
         <div className="idea-section-header">Why?</div>
         <div className="idea-section-text">{context}</div>
+      </div>
+      <div className="idea-upvote-button-container">
+        <IconContext.Provider value={{ color: upvoteIsActive ? '#1DA1F2' : '#ffffff', size: '3rem' }}>
+          <IoArrowUpCircleOutline
+            className="idea-upvote-button"
+            onClick={handleUpvoteClick}
+          />
+        </IconContext.Provider>
       </div>
     </div>
   );
