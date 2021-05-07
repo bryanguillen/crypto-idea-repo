904f2505335924f23e40e01ebab5bfb5e7376d93
bryg217 <bryan.guillen217@gmail.com>
2021-05-06 21:57:28 -0400

    feat: add upvotes to list of data of returned for selectIdeas

--- back-end/src/endpoints/idea/idea.queries.js
+++ back-end/src/endpoints/idea/idea.queries.js
@@ -100,7 +100,7 @@ async function selectIdeas(idForLastIdeaSeen) {
     idForLastIdeaSeen
   ];
   const queryResults = await db.query(`
-  SELECT description, context, title, id FROM idea
+  SELECT description, context, title, id, upvotes FROM idea
   WHERE id < $1
   ORDER BY id DESC
   LIMIT 10;
